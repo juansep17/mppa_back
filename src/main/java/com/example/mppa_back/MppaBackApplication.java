8cf8c67c6daf4bc52e892dbcef30c7f98e5bcafe
Wilman Guarin <wguari1@bancodebogota.com.co>
2020-03-31 14:54:16 -0500

    Creación proyecto back para MPPA

---
+++ src/main/java/com/example/mppa_back/MppaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mppa_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mppa_back/MppaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mppa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MppaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
